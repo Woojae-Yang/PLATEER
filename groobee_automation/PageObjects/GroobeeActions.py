b082abd690a91adb5a42f07d18551bbf911cefad
namgp01 <gpnam1@naver.com>
2026-02-11 09:22:21 +0900

    2026-02-11 09:22

--- groobee_automation/PageObjects/GroobeeActions.py
+++ groobee_automation/PageObjects/GroobeeActions.py
@@ -157,12 +157,14 @@ class GroobeeActions:
         el = BaseClass.wait_visible(self.driver, self.cam_name, timeout)
         el.click()
         el.send_keys(Keys.CONTROL, "a")
+        el.send_keys(Keys.COMMAND, "a")
         el.send_keys(Keys.BACKSPACE)
         el.send_keys(text)
     def send_cam_des(self, text, timeout=10):
         el = BaseClass.wait_visible(self.driver, self.cam_des, timeout)
         el.click()
         el.send_keys(Keys.CONTROL, "a")
+        el.send_keys(Keys.COMMAND, "a")
         el.send_keys(Keys.BACKSPACE)
         el.send_keys(text)
     def get_cam_name(self, timeout=10):
